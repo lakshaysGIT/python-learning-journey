e438dbdd7169216659e8e67f1c09884005b3fa89
Lakshay Batra <lakshaybatra41@yahoo.com>
2026-03-19 14:57:56 +0530

    some new commands

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 print("lakshay is now coding!")
 print("VSCode linked to GitHub")
 print("lakshay is now coding! and VSCode linked to GitHub")
+
+
+print("Hello World")
+name=input("What's your name,Buddy?")
+print ("Nice to see you",name)
+print (name,"Batra",name,"Batra",name,"Batra")
