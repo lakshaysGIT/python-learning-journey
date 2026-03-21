96277022485ea240cafc20e3ffd2343a7e47f87f
Lakshay Batra <lakshaybatra41@yahoo.com>
2026-03-21 23:21:14 +0530

    Variables,datatypes,operators

--- test.py
+++ test.py
@@ -1,9 +1,67 @@
-print("lakshay is now coding!")
-print("VSCode linked to GitHub")
-print("lakshay is now coding! and VSCode linked to GitHub")
+# print("lakshay is now coding!")
+# print("VSCode linked to GitHub")
+# print("lakshay is now coding! and VSCode linked to GitHub")
 
+# #taking input from user
 
-print("Hello World")
-name=input("What's your name,Buddy?")
-print ("Nice to see you",name)
-print (name,"Batra",name,"Batra",name,"Batra")
+# print("Hello World")
+# name=input("What's your name,Buddy?")
+# print ("Nice to see you",name)
+# print (name,"Batra",name,"Batra",name,"Batra")
+
+##-------------------------
+
+## Lets start with Vairables and operators
+# myint=10
+# myfloat=1.123
+# mybool=True
+# mystr="LakshayBatra"
+# print(mybool)
+# print(mystr)
+
+# #operations on variables
+
+# print(myint*myfloat)
+# print(myint%myfloat)
+# print(myint-myfloat)
+# print(myint+myfloat)
+
+# anotherstr="@yahoo.com"
+# print(mystr+anotherstr)
+# print("L"*3)
+
+# #Logical and comparison operators
+# print(myint==10)
+# print(myfloat!=20)
+# print(myint>1000000)
+# print(mybool==True)
+# print(mystr=="LakshayBatra")
+
+# print(myint>5 and myfloat<2000)
+# print(myint>100 or mybool==True)
+# print(myint!=10 or myfloat>100)
+
+# #we can re-declare the previously used variable and override
+# myint="STRING"
+# print("Now it will print STRING:",myint)
+
+##Sequence: Lists and Tuples
+##can contain multiple values separated by comas any datatype
+
+mylist=[0,1,2,3,True]
+# print(len(mylist)) #check length of list
+# print(mylist[0])
+# print(mylist[-1]) #minus means starts from reverse
+# print(mylist[4])
+# mylist[4]=False #assigning new value
+# print(mylist)
+
+# anotherlist=[False,7,8]
+# print(anotherlist)
+# anotherlist[0]=True
+# print(anotherlist)
+# new_my=mylist+anotherlist # adding both lists
+# print(new_my)
+
+mystr=str("LAKSHAY")
+print(mystr[-1])
